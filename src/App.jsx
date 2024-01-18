fa1e71f821e23dcebeb987485d260af32d11eeed
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2024-01-18 17:17:57 +0600

    finished assginment added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/header/Navbar";
+import Main from "./components/main/Main";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
